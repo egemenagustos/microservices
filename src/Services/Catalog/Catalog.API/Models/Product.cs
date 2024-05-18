05676f4bf1f5562a4b162c274536d2b75b6ee858
Egemen Agustos <info@egemenagustos.com>
2024-05-18 22:19:24 +0300

    Mediatr has been added and configured. Marten and Carten libraries were added.

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,45 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        /*
+        Default! = Burada default! ifadesi şu anlama gelir: 
+
+        Default kelimesi, o tür için varsayılan değeri döndürür. Bir string için varsayılan değer null'dır. 
+
+        !(null-forgiving operator) operatörü ise, derleyiciye:
+
+        "Bu değerin null olabileceğini biliyorum ama bu durumu önemsemiyorum, kontrol etme" demenin bir yoludur. 
+
+        Bu kullanımıyla default!, Description ve ImageFile özelliklerinin başlangıç değerlerinin null olabileceğini belirtiyor
+
+        ancak bu özelliklerin ileride mutlaka bir değere atanacağını garanti ediyor.
+        */
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<string> Category { get; set; }
+
+        public string Description { get; set; } = default!;
+
+        public string ImageFile { get; set; } = default!;
+
+        public decimal Price { get; set; }
+
+        public Product()
+        {
+            Category = new List<string>();
+        }
+
+        public Product(string name, List<string> category, string description, string imageFile, decimal price)
+        {
+            Name = name;
+            Category = category;
+            Description = description;
+            ImageFile = imageFile;
+            Price = price;
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            Product product = new(
+                name: request.Name,
+                category: request.Category,
+                description: request.Description,
+                imageFile: request.ImageFile,
+                price: request.Price
+                );
+
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+            return new(product.Id);
+        }
+    }
+}
