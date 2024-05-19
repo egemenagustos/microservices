a19c78e1375e7798deb30663df380d86bbb04150
Egemen Agustos <info@egemenagustos.com>
2024-05-19 10:39:06 +0300

    developed product fetch endpoint based on id.

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,27 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    //public record GetProductByIdQuery(Guid Id) :
+    //    IQuery<GetProductByIdResult>;
+
+    public record GetProductByIdResponse(Product Product);
+
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var product = await sender.Send(new GetProductByIdQuery(id));
+
+                var response = product.Adapt<GetProductByIdResponse>();
+
+                return Results.Ok(response);
+            })
+              .WithName("GetProductById")
+              .Produces<GetProductByIdResponse>(StatusCodes.Status201Created)
+              .ProducesProblem(StatusCodes.Status400BadRequest)
+              .WithSummary("Get Product By Id")
+              .WithDescription("Get Product By Id");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) :
+        IQuery<GetProductByIdResult>;
+
+    public record GetProductByIdResult(Product Product);
+
+    public class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger) :
+        IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called by with {@Query}", request);
+
+            var product = await session.LoadAsync<Product>(request.Id,cancellationToken);
+
+            if (product is null)
+                throw new ProductNotFoundException();
+
+            return new(product);
+        }
+    }
+}
