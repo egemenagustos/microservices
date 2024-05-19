1584f8396867b3548dac00d895ae9c6b2c1f5743
Egemen Agustos <info@egemenagustos.com>
2024-05-19 14:35:13 +0300

    product deletion endpoint has been improved.

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id) : 
+        ICommand<DeleteProductCommandResult>;
+
+    public record DeleteProductCommandResult(bool IsSuccess);
+
+    public class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommandHandler> logger) :
+        ICommandHandler<DeleteProductCommand, DeleteProductCommandResult>
+    {
+        public async Task<DeleteProductCommandResult> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("DeleteProductCommandHandler.Handle called by with {@Query}", request);
+
+            var product = await session.LoadAsync<Product>(request.Id, cancellationToken);
+
+            if (product is null)
+                throw new ProductNotFoundException();
+
+            session.Delete<Product>(request.Id);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new(true);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,28 @@
+﻿namespace Catalog.API.Products.DeleteProduct
+{
+    //public record DeleteProductRequest(Guid Id) :
+    //    ICommand<DeleteProductCommandResult>;
+
+    public record DeleteProductCommandResponse(bool IsSuccess);
+
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result =  await sender.Send(new DeleteProductCommand(id));
+
+                var response = result.Adapt<DeleteProductCommandResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("DeleteProduct")
+                .Produces<DeleteProductCommandResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Delete Product")
+                .WithDescription("Delete Product");
+        }
+    }
+}
