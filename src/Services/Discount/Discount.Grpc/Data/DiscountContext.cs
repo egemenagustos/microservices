c4a2d3ee3013c3f34895ff1d3c5148eb4b322ed4
Egemen Agustos <info@egemenagustos.com>
2024-05-22 18:34:17 +0300

    sql lite has been added and proto files have been edited.

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,36 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext : DbContext
+    {
+        public DiscountContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>()
+                .HasData(
+                new Coupon()
+                {
+                    Id = 1,
+                    ProductName = "Iphone X",
+                    Amount = 100,
+                    Description = "Iphone Discount"
+                },
+                new Coupon()
+                {
+                    Id = 2,
+                    ProductName = "Samsung 10",
+                    Amount = 102,
+                    Description = "Samsung Discount"
+                }
+                );
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
